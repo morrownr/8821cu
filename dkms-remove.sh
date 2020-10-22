bddb8a2a5226aa22e83645cb58e0aa37728785d5
morrownr <morrownr@gmail.com>
2020-10-22 00:45:42 -0500

    fix logic error

--- dkms-remove.sh
+++ dkms-remove.sh
@@ -1,23 +1,22 @@
 #!/bin/bash
 
+DRV_NAME=rtl8821cu
+DRV_VERSION=5.8.1.4
+
 if [[ $EUID -ne 0 ]]; then
   echo "You must run this installation script with superuser priviliges."
   echo "Try \"sudo ./dkms-remove.sh\""
   exit 1
 fi
 
-DRV_NAME=rtl8821cu
-DRV_VERSION=5.8.1.4
-
 dkms remove ${DRV_NAME}/${DRV_VERSION} --all
-rm -rf /usr/src/${DRV_NAME}-${DRV_VERSION}
-
 RESULT=$?
 
 if [[ "$RESULT" != "0" ]]; then
-  echo "An error occurred while running dkms-remove.sh."
+  echo "An error occurred while running dkms remove"
+  exit $RESULT
 else
-  echo "dkms-remove.sh was successful."
+	rm -f /etc/modprobe.d/88x2bu.conf
+	rm -rf /usr/src/${DRV_NAME}-${DRV_VERSION}
+	echo "dkms-remove.sh was successful."
 fi
-
-exit $RESULT
