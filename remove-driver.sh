f279948825e7dbaaa1665a7c29472d85a338c6a0
morrownr <morrownr@gmail.com>
2020-12-22 14:08:32 -0600

    minor updates

--- remove-driver.sh
+++ remove-driver.sh
@@ -4,7 +4,8 @@ DRV_NAME=rtl8821cu
 DRV_VERSION=5.8.1.7
 OPTIONS_FILE=8821cu.conf
 
-SCRIPT_NAME=remove-driver.sh
+KRNL_VERSION=$(uname -r)
+SCRIPT_NAME="remove-driver.sh"
 
 if [[ $EUID -ne 0 ]]; then
 	echo "You must run this script with superuser (root) privileges."
@@ -12,16 +13,15 @@ if [[ $EUID -ne 0 ]]; then
 	exit 1
 fi
 
-rm -f /etc/modprobe.d/${OPTIONS_FILE}
-rm -rf /usr/src/${DRV_NAME}-${DRV_VERSION}
-
-dkms remove ${DRV_NAME}/${DRV_VERSION} --all
+dkms remove ${DRV_NAME}/${DRV_VERSION} -k ${KRNL_VERSION}
 RESULT=$?
 
 if [[ "$RESULT" != "0" ]]; then
 	echo "An error occurred while running: dkms remove : ${RESULT}"
 	exit $RESULT
 else
+	rm -f /etc/modprobe.d/${OPTIONS_FILE}
+	rm -rf /usr/src/${DRV_NAME}-${DRV_VERSION}
 	echo "The driver was removed successfully."
 	exit 0
 fi
